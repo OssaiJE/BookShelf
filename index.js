b418aa83908bebfce462b51a4473d4707e990a13
OssaiJE <sirjuliusdev@gmail.com>
2020-10-29 14:29:33 +0100

    added some helpers to improve functionality, now stories are viewed by all users and the edit features is specific users

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Handlebars Helpers
-const {formatDate} = require("./helpers/hbs");
+const {formatDate, stripTags, truncate, editIcon} = require("./helpers/hbs");
 
 // Handle-bars extension and the layout
-app.engine('.hbs', exphbs({helpers: {formatDate}, defaultLayout: "main", extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {formatDate, stripTags, truncate, editIcon}, defaultLayout: "main", extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -47,6 +47,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global Variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
