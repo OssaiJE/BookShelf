8b21c7b93ad143527b3cf320e16ab15494d654a8
OssaiJE <sirjuliusdev@gmail.com>
2020-11-03 15:37:55 +0100

    i decided to remove the method overide shit...still everything works...i've got no need for it

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
-const methodOverride = require('method-override');
+// const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const mongoStore = require("connect-mongo")(session);
@@ -26,14 +26,14 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // Method Override
-app.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
+// app.use(methodOverride(function (req, res) {
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     var method = req.body._method;
+//     delete req.body._method;
+//     return method;
+//   }
+// }));
 
 // Logging
 if (process.env.NODE_ENV === "development") {
