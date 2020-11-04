9cd7fd0ecada21ec62e534c1cb397e25f73d804f
OssaiJE <sirjuliusdev@gmail.com>
2020-11-04 16:53:08 +0100

    was able to get the delete button working...as at my knowledge, everything works perfectly

--- routes/stories.js
+++ routes/stories.js
@@ -43,39 +43,62 @@ router.get("/", ensureAuth, async (req, res) => {
 // @desc Show edit page
 // @route Get /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-    const story = await Story.findOne({
-        _id: req.params.id
-    }).lean();
-
-    if (!story) {
-        return res.render("error/404");
-    }
-
-    if (story.user != req.user.id) {
-        res.redirect("/stories");
-    } else {
-        res.render("stories/edit", {
-            story
-        });
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean();
+    
+        if (!story) {
+            return res.render("error/404");
+        }
+    
+        if (story.user != req.user.id) {
+            res.redirect("/stories");
+        } else {
+            res.render("stories/edit", {
+                story
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render("error/500")
     }
 });
 
 // @desc Update story
-// @route PUT /stories/:id
+// @route POST /stories/:id
 router.post("/:id", ensureAuth, async (req, res) => {
-    let story = await Story.findById(req.params.id).lean();
-    if (!story) {
-        return res.render("error/404");
-    }
-    if (story.user != req.user.id) {
-        res.redirect("/stories");
-    } else {
-        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true,
+    try {
+        let story = await Story.findById(req.params.id).lean();
+        if (!story) {
+            return res.render("error/404");
+        }
+        if (story.user != req.user.id) {
+            res.redirect("/stories");
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true,
         });
-        res.redirect("/dashboard");
+            res.redirect("/dashboard");
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render("error/500");
     }
 });
+
+// @desc Delete stories
+// @route DELETE /stories/delete/:id
+router.get("/delete/:id", ensureAuth, async (req, res) => {
+    try {
+        await Story.findByIdAndRemove({_id: req.params.id});
+        res.redirect("/dashboard");
+    } catch (err) {
+        console.error(err);
+        return res.render("/error/500");
+    }
+});
+
 
 module.exports = router;
